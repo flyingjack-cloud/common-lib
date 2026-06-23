5037addf2bfbcf3d8e1c35669741fb9b40be7c27
Zumin Li <zumin.li.work@gmail.com>
2026-06-23 20:04:39 +0800

    1.fix jackson bug

---
+++ src/main/java/top/flyingjack/common/config/JacksonAutoConfiguration.java
@@ -0,0 +1,18 @@
+package top.flyingjack.common.config;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonAutoConfiguration {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer longToStringSerializer() {
+        return builder -> {
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+            builder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+        };
+    }
+}
